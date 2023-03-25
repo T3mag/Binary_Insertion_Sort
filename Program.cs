7f0188165a00c09ba3b6c21a77e1dacab604b15a
T3mag <aminnushin@list.ru>
2023-03-25 14:24:36 +0300

    Work version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Binary_Insortion_Sort;
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Binary_Insortion_Sort;
 
 public static class Sort
 {
@@ -6,22 +9,21 @@ public static class Sort
     {
         public static void Main()
         {
-            var mas = new int[] {1,2,3,4,5,7,2,10,5};
-            
-            for (int i = 0; i < mas.Length; i++)
-                Console.Write(mas[i] + " ");
+            string inputFile = "/Users/arturminnusin/RiderProjects/Binary Insertion Sort/bin/Debug/net7.0/Data";
+            string outputFile = "/Users/arturminnusin/RiderProjects/Binary Insertion Sort/bin/Debug/net7.0/SortArray.txt";
+            string diagnosticFile = "/Users/arturminnusin/RiderProjects/Binary Insertion Sort/bin/Debug/net7.0/Diagnostic.txt";
 
-            Console.WriteLine();
-            
-            var mas1 = BinaryInsortionSort(mas);
-
-            for (int i = 0; i < mas1.Length; i++)
-                Console.Write(mas1[i] + " ");
+            GenerateDataInFile(inputFile);
+            SortAllArrays(inputFile, outputFile, diagnosticFile);
         }
         
         public static int[] BinaryInsortionSort(int[] mas)
         {
-            for (int i = 1; i < mas.Length; i++)
+            Stopwatch time = new Stopwatch();
+            var iter = 0;
+            
+            time.Start();
+            for (int i = 0; i < mas.Length; i++)
             {
                 var low = 0;
                 var high = i - 1;
@@ -29,6 +31,7 @@ public static class Sort
 
                 while (low <= high)
                 {
+                    iter++;
                     int mid = (low + high) / 2;
 
                     if (cur < mas[mid])
@@ -38,13 +41,85 @@ public static class Sort
                 }
 
                 for (int j = i - 1; j >= low; j--)
+                {
                     mas[j + 1] = mas[j];
+                    iter++;
+                }
                 
                 mas[low] = cur;
             }
 
-            return mas;
+            iter += mas.Length;
+            time.Stop();
+
+            var x = new int[3] {mas.Length, iter, (int)time.ElapsedTicks};
+
+            return x;
+        }
+
+        public static void SortAllArrays(string input, string output ,string diagnostic)
+        {
+            StreamReader file = new StreamReader(input);
+            StreamWriter file1 = new StreamWriter(output);
+            StreamWriter fileD = new StreamWriter(diagnostic);
+            while (!file.EndOfStream)
+            {
+                var line = file.ReadLine().Split(",");
+                var a = new int[line.Length];
+                
+                for (int i = 0; i < a.Length; i++)
+                    a[i] = int.Parse(line[i]);
+               
+                var d = BinaryInsortionSort(a);
+
+                foreach (var item in d)
+                {
+                    fileD.Write(item + ",");
+                }
+                fileD.WriteLine();
+                
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a.Length - i != 0)
+                        file1.Write(a[i] + ",");
+                    else
+                        file1.Write(a[i]);
+                }
+                
+                file1.WriteLine();
+                file1.WriteLine("------------------------------------------------------------");
+            }
+            file.Close();
+            file1.Close();
+            fileD.Close();
         }
         
+        public static void GenerateDataInFile(string fileName)
+        {
+            if (fileName == null)
+                throw new ArgumentException("Нет имени файла!");
+        
+            int counter1 = 0;
+            int counter2 = 100;
+            StreamWriter file = new StreamWriter(fileName);
+            Random r = new Random();
+        
+            while (counter1 < 100)
+            {
+                for (int i = 0; i < counter2; i++)
+                {
+                    var item = r.Next(1, 10);
+                    if(counter2 - i != 1)
+                        file.Write(item + ",");
+                    else
+                        file.Write(item);
+                
+                }
+                file.WriteLine();
+                counter1++;
+                counter2 += 99;
+            }
+            file.Close();
+        }
     }
 }
